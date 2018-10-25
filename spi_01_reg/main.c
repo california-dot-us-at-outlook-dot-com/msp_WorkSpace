dfaf7a375c1cec8fa1907d5dd985e1df6751135d
cal <california.us@outlook.com>
2018-10-25 09:14:17 +0800

    fix spi_reg

--- spi_01_reg/main.c
+++ spi_01_reg/main.c
@@ -8,9 +8,9 @@
 void init_spi(){
     U0CTL |= SWRST;
     U0CTL |= CHAR+SYNC+MM;
-    U0TCTL |= SSEL1+SSEL0+STC;
-    U0TCTL=CKPH;
-    U0TCTL &= ~CKPL;
+    U0TCTL |= SSEL1+SSEL0+STC+CKPH;
+    //U0TCTL=CKPH;
+    //U0TCTL &= ~CKPL;
     U0BR0=0x02;
     U0BR1=0x00;
     ME1 |= USPIE0;
@@ -18,13 +18,13 @@ void init_spi(){
     IE1&=~UTXIE0;
     IE1&=~URXIE0;
     P3SEL|=0x0e;
-    P3DIR|=BIT2+BIT4;
+    P3DIR|=0x01;
 }
 
 void spi_sendByte(unsigned char dat){
     U0TXBUF=dat;
     while((IFG1&UTXIFG0)==0);
-    IFG1&=~UTXIFG0;
+    //IFG1&=~UTXIFG0;
 }
 
 void delay(void){
@@ -54,7 +54,7 @@ void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	unsigned char i;
-	unsigned char data[100];
+	unsigned char data[10]={9,8,7,6,5,4,3,2,1,0};
 	Clock_Init();
 	init_spi();
 	while(1){
