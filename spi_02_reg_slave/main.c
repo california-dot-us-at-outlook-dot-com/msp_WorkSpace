47c7165b0b499d40fac090e2f376377435ee6457
cal <california.us@outlook.com>
2018-10-25 16:22:57 +0800

    add spi_02_reg_slave

---
+++ spi_02_reg_slave/main.c
@@ -0,0 +1,28 @@
+#include <msp430f169.h>
+
+
+void init_spi_slave(){
+      P3SEL = 0x0E;                             // Setup P3 for SPI mode
+      U0CTL = CHAR + SYNC + SWRST;              // 8-bit, SPI, Slave
+      U0TCTL = CKPL + STC;                      // Polarity, UCLK, 3-wire
+      ME1 = USPIE0;                             // Module enable
+      U0CTL &= ~SWRST;                          // SPI enable
+      IE1 |= URXIE0;                            // Recieve interrupt enable
+      _EINT();                                  // Enable interrupts
+
+}
+
+#pragma vector=USART0RX_VECTOR
+__interrupt void SPI0_rx (void){
+    unsigned char data=0;
+    data=U0RXBUF;
+    P6OUT=~data;
+}
+
+void main(void)
+{
+    init_spi_slave();
+    P6DIR=0xff;
+    P6OUT=0xff;
+    while(1);
+}
